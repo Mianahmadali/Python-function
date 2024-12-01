e4f18581ffc86caa3cbaf4f7f583e07d5d6aee44
Mian Ahmad Ali <mianahmadaliali123@gmail.com>
2024-12-01 17:19:33 +0500

    Add files via upload
    This repository contains a collection of reusable Python functions designed to simplify common programming tasks. The functions are organized by category and include detailed documentation and examples for easy integration into your projects." Mathematical Functions: Perform complex calculations with ease.
    String Manipulation: Efficiently handle and transform strings.
    Data Processing: Functions for cleaning and processing data.
    File Operations: Simplify reading from and writing to files.

---
+++ Q4.py
@@ -0,0 +1,7 @@
+#Write a function that takes a string and returns it reversed.
+def reversed_string(s):
+     return s[::-1]
+
+s="hello Ahmad"
+result= reversed_string(s)
+print(f"The reserved string {s} is { result}")
